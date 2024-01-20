cebce4a69595dd59c516f41ed311e19bfd3db37f
carlocodes <Gian Uy>
2024-01-20 18:02:11 +0800

    initial commit

---
+++ src/main/java/com/carlocodes/bankapigatewayservice/BankApiGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.carlocodes.bankapigatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApiGatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankApiGatewayServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/carlocodes/bankapigatewayservice/BankApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlocodes.bankapigatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApiGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
